e3fafc8169c59ce36b3dc59a0ab2961729783668
ednaldo.junior <ednaldo.junior@prill.com.br>
2025-08-26 16:41:17 -0300

    feat: implements DELETE city

--- src/main/java/com/devsuperior/bds02/controller/CityController.java
+++ src/main/java/com/devsuperior/bds02/controller/CityController.java
@@ -4,7 +4,9 @@ import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.services.CityService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +32,11 @@ public class CityController {
     @PostMapping
     public ResponseEntity<CityDTO> insert(@RequestBody CityDTO dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(cityService.insert(dto));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        cityService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -3,11 +3,15 @@ package com.devsuperior.bds02.services;
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.services.exceptions.DatabaseException;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
+@Transactional
 @Service
 public class CityService {
 
@@ -28,5 +32,18 @@ public class CityService {
         entity = cityRepository.save(entity);
 
         return new CityDTO(entity);
+    }
+
+    public void delete(Long id) {
+        if (!cityRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Resource Not Found Id: " + id);
+        }
+
+        try {
+            cityRepository.deleteById(id);
+
+        }catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial:");
+        }
     }
 }
