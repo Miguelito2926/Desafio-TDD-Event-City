1d482dbd53e2c96367d84a41eefb6cde1ea39115
ednaldo.junior <ednaldo.junior@prill.com.br>
2025-08-26 15:57:57 -0300

    feat: implements POST insert Cities.

--- src/main/java/com/devsuperior/bds02/controller/CityController.java
+++ src/main/java/com/devsuperior/bds02/controller/CityController.java
@@ -2,8 +2,11 @@ package com.devsuperior.bds02.controller;
 
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.services.CityService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +25,10 @@ public class CityController {
     @GetMapping
     public ResponseEntity<List<CityDTO>> findAll() {
         return ResponseEntity.ok(cityService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<CityDTO> insert(@RequestBody CityDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(cityService.insert(dto));
     }
 }
