6446b1056edc801d30a5db99b12f103dec8f7572
ednaldo.junior <ednaldo.junior@prill.com.br>
2025-08-26 15:49:30 -0300

    feat: implements API Cities.

---
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.bds02.services;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CityService {
+
+    private final CityRepository cityRepository;
+
+    public CityService(CityRepository cityRepository) {
+        this.cityRepository = cityRepository;
+    }
+
+    public List<CityDTO> findAll() {
+        List<City> list = cityRepository.findAll(Sort.by("name"));
+        return list.stream().map(CityDTO::new).toList();
+    }
+}
